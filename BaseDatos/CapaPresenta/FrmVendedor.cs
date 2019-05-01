90a944d08cb20693acda8f12312c3a0a3737b0fd
Gared94 <47989107+Gared94@users.noreply.github.com>
2019-04-30 19:59:47 -0600

    Practica 30 de Abril
    Practica 30 de Abril

---
+++ BaseDatos/CapaPresenta/FrmVendedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocio;
+namespace CapaPresenta
+{
+    public partial class FrmVendedor : Form
+    {
+        ManttoVendor mntt = new ManttoVendor();
+        public FrmVendedor()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVendedor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
